91d234fa83c764c2723410e7f0a8409250631391
ACCESS  DAVID SAVEDRA <EDSC@ahtl.local>
2025-02-07 12:52:48 +0100

    updateAlinAp

--- Program.cs
+++ Program.cs
@@ -5,27 +5,25 @@ using MauiApp1.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-
-builder.Services.AddScoped<AlbaranService>();
-builder.Services.AddScoped<LineaAlbaranService>();
-
-//new
 builder.Services.AddScoped<IAlbaranService, AlbaranService>();
 builder.Services.AddScoped<ILineaAlbaranService, LineaAlbaranService>();
 builder.Services.AddScoped<IClienteService, ClienteService>();
 builder.Services.AddScoped<IMercanciaService, MercanciaService>();
 builder.Services.AddScoped<IRepartidorService, RepartidorService>();
 
-
-
-
+// ?? CONFIGURACIÓN DE CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy => policy.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader());
+});
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -42,8 +40,11 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+// ?? HABILITAR CORS ANTES DE AUTHORIZATION
+app.UseCors("AllowAll");
+
 app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();  
+app.Run();
